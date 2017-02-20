000baa7fe19d6c8098a4d7a5fe0c81d762f1e30d
raydon <gerdong@126.com>
2017-02-20 21:45:31 +0800

    modify restful api

--- www/restful/lan.py
+++ www/restful/lan.py
@@ -3,46 +3,60 @@ import json
 from db import *
 
 
+
+
+
+
+user_identity = {
+					'gerundong-ipad':{'user':'gerundong', 'threshold': 7},
+					'gerundong-iphone':{'user':'gerundong', 'threshold': 0},
+					'lixianghui-ipad':{'user':'lixianghui', 'threshold': 7},
+					'lixianghui-iphone':{'user':'lixianghui', 'threshold': 0},
+					'sunlili-ipad':{'user':'sunlili', 'threshold': 7},
+					'sunlili-iphone':{'user':'sunlili', 'threshold': 0},
+				
+				}
+
+
 #replace with memcached later
-def getdbdata(lanid, bengin, end):
+def getdbdata(lanid, begin, end):
 	result = {}
-	for record in Lan:
+	for record in Lan.select().where(((Lan.time >= begin) & (Lan.time <= end))):
 		result[record.time.strftime("%Y-%m-%d %H:%M:%S")] = json.loads(record.user)
 	return result
 
-
-
-
-def getrawdata(selectedday):
-	formatday = selectedday.replace(microsecond = 0, second = 0, minute =0, hour=0)
-	week = int(formatday.strftime("%W")) + 1
-	weekday = int(formatday.strftime("%w"))
-	monday = (weekday == 0) and formatday - timedelta(days = 6) or formatday - timedelta(days = weekday - 1) 
-	sunday = monday + timedelta(days = 6)
+def get_one_week_userdata_tenmin(lanid, username, begin):
+	begindate = begin.replace(microsecond = 0, second = 0, minute =0, hour=0)
+	enddate = begindate + timedelta(days = 6, hours = 23, minutes = 50)
 	result = {}
-	result['begin'] = monday.strftime('%Y-%m-%d %H:%M:%S')
-	result['end'] = sunday.strftime('%Y-%m-%d %H:%M:%S')
-	result['week'] = week
-
-	dbdata = getdbdata(1,1,1)
+	result['begin'] = begindate.strftime('%Y-%m-%d %H:%M:%S')
+	result['end'] = enddate.strftime('%Y-%m-%d %H:%M:%S')
+	dbdata = getdbdata(lanid, begindate, enddate)
 	for index in range(0,6*24*7):
-		#result[str(index)] = {'t':(monday + timedelta(minutes = 10) * index).strftime('%Y-%m-%d %H:%M:%S'), 'v':0}	
-		t = (monday + timedelta(minutes = 10) * index).strftime('%Y-%m-%d %H:%M:%S')
+		t = (begindate + timedelta(minutes = 10) * index).strftime('%Y-%m-%d %H:%M:%S')
 		totalhit = 0
 		if not dbdata.has_key(t):
 			result[t] = 0
 			continue
-		for userindex in range(0,dbdata[t]['usernum']):
-			totalhit = dbdata[t]['hitcount'][userindex] + totalhit
-		totalhit = totalhit >= 15 and totalhit -15 or 0
-		result[t] = totalhit 
+		for i,v in enumerate(dbdata[t]['username']):
+			if not user_identity.has_key(v):
+				continue
+			user = user_identity[v]['user']
+			threshold = user_identity[v]['threshold']
+			if user != username:
+				continue
+			hitcount = dbdata[t]['hitcount'][i]
+			if hitcount < threshold:
+				hitcount = 0
+			totalhit = hitcount + totalhit
+		result[t] = totalhit
 	return result
+
 
 def merge2halfhour(rawdata):
 	result = {}
 	result['begin'] = rawdata['begin']
 	result['end'] = (datetime.strptime(rawdata['end'], ('%Y-%m-%d %H:%M:%S')) + timedelta(minutes = 30, hours = 23)).strftime('%Y-%m-%d %H:%M:%S') 
-	result['week'] = rawdata['week']
 	begin = datetime.strptime(rawdata['begin'], ('%Y-%m-%d %H:%M:%S')) 
 	v = 0
 	for index in range(0,6*24*7):
@@ -54,10 +68,10 @@ def merge2halfhour(rawdata):
 			v = 0 
 	return result
 	
-def getweek(selectedday):
-	result = merge2halfhour(getrawdata(selectedday))
-	return json.dumps(merge2halfhour(getrawdata(selectedday)))
+def getweek(lanid, username, begin):
+	begin = datetime.strptime('2017-02-20 00:00:00', ('%Y-%m-%d %H:%M:%S'))
+	return json.dumps(merge2halfhour(get_one_week_userdata_tenmin('304', username, begin)))
 
 if __name__ == "__main__":
 
-	print getweek(datetime.now())
+	print (getweek('304', 'gerundong', 1))
